b867916fcc6c13c79a73fccc0be597ed337cb663
Weston Maggetti <weston.maggetti@gmail.com>
2021-01-03 16:03:29 -0500

    2nd

--- first.js
+++ first.js
@@ -1,3 +1,10 @@
 var catBreed;
 catBreed = "manx";
 console.log(catBreed);
+var list = [1, 2, 3];
+console.log(list[0]);
+for (var _i = 0, list_1 = list; _i < list_1.length; _i++) {
+    var i = list_1[_i];
+    console.log(i);
+    console.log("====");
+}
--- first.ts
+++ first.ts
@@ -3,3 +3,17 @@ var catBreed: String;
 catBreed = "manx";
 
 console.log(catBreed)
+
+let list: number[] = [1,2,3];
+console.log(list[0]);
+
+for (let i of list)
+{
+    console.log(i);
+    console.log("====")
+}
+let stringList: string[] =["asd","asd2"];
+for (let i of stringList)
+{
+    console.log(i);
+}
